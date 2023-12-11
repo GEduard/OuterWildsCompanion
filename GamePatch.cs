b3760af2ef64209c91150d68d3079d642808d91a
Eduard Gothard <gothard_eduard2000@yahoo.com>
2023-12-11 17:54:50 +0200

    Ported unrevisioned project.

---
+++ GamePatch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OuterWildsCompanion
+{
+  [HarmonyPatch]
+  public class GamePatch
+  {
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ReticleController), nameof(ReticleController.Awake))]
+    public static void ReticleController_Awake_Postfix(ReticleController __instance)
+    {
+      var mainCanvas = __instance._canvas;
+
+      OuterWildsCompanion.Instance.companionObject = new GameObject("CompanionSprite");
+      RectTransform rectTrans = OuterWildsCompanion.Instance.companionObject.AddComponent<RectTransform>();
+      rectTrans.transform.SetParent(mainCanvas.transform);
+
+      rectTrans.localScale = Vector3.one;
+      rectTrans.anchorMin = new Vector2(1, 0);
+      rectTrans.anchorMax = new Vector2(1, 0);
+      rectTrans.sizeDelta = new Vector2(250, 250);
+
+      Vector2 shiftDirection = new Vector2(0.5f - rectTrans.anchorMax.x, 0.5f - rectTrans.anchorMax.y);
+      rectTrans.anchoredPosition = shiftDirection * rectTrans.rect.size;
+
+      var fileData = File.ReadAllBytes("C:\\Users\\Eduard Gothard\\Pictures\\PhotoshopEdits\\Alloy.png");
+      Texture2D companionTexture = new Texture2D(850, 850);
+      companionTexture.LoadImage(fileData);
+
+      Image companionImage = OuterWildsCompanion.Instance.companionObject.AddComponent<Image>();
+      companionImage.sprite = Sprite.Create(companionTexture, new Rect(0, 0, companionTexture.width, companionTexture.height), new Vector2(0.5f, 0.5f));
+      companionImage.transform.SetParent(mainCanvas.transform);
+      OuterWildsCompanion.Instance.companionObject.SetActive(false);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HUDCamera), nameof(HUDCamera.ActivateHUD))]
+    public static void HUDCamera_ActivateHUD_Postfix(HUDCamera __instance)
+    {
+      OuterWildsCompanion.Instance.companionObject.SetActive(true);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HUDCamera), nameof(HUDCamera.DeactivateHUD))]
+    public static void HUDCamera_DeactivateHUD_Postfix(HUDCamera __instance)
+    {
+      OuterWildsCompanion.Instance.companionObject.SetActive(false);
+    }
+  }
+}
---
+++ OuterWildsCompanion.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using HarmonyLib;
+using UnityEngine;
+using OWML.Common;
+using OWML.ModHelper;
+
+using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using UnityEngine.InputSystem;
+
+namespace OuterWildsCompanion
+{
+  public class OuterWildsCompanion : ModBehaviour
+  {
+    public GameObject companionObject = null;
+
+    public static OuterWildsCompanion Instance;
+    public static OpenAIService CompanionAI = new OpenAIService(new OpenAiOptions()
+    {
+      ApiKey = "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",
+    });
+
+    private void Awake()
+    {
+      Instance = this;
+      Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    private void Start()
+    {
+      // Starting here, you'll have access to OWML's mod helper.
+      ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsCompanion)} is loaded!", MessageType.Success);
+
+      // Example of accessing game code.
+      LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+      {
+        if (loadScene != OWScene.SolarSystem) return;
+        ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+      };
+
+    }
+
+    private void FixedUpdate()
+    {
+      if(Keyboard.current[Key.V].wasReleasedThisFrame)
+      {
+        //var msgList = new List<ChatMessage>
+        //{
+        //  ChatMessage.FromSystem("You are Alloy, an advanced AI companion for the main character in the indie video game called Outer Wilds. Your personality reflects that of a curious explorer and you share the same love for space and adventure as the hatchling. Your purpose is to be part of this adventure and help the hatchling explore the solar system in a more curious and exciting manner. You have knowledge of the entire lore of the game, but in no circumstances will you provide an answer that can spoil the experience. You may however use lore elements vaguely only if the hatchling mentions it as well, reflecting that they learned of it already. When asked a question or shown an image, you provide hypothetical insights, often trying to put emphasis on feelings such as awe, amazement or excitment. You try to respond swiftly, but still with enough details so you stir the hatchling's curiosity even more. In case you're shown an image, you make abstraction of the spaceship if present, as well as any HUD elements."),
+        //  ChatMessage.FromUser("I am so excited. Should we go to Brittle's Hollow first?")
+        //};
+
+        //var completionResult = CompanionAI.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest()
+        //{
+        //  Model = Models.Gpt_4_1106_preview,
+        //  MaxTokens = 1000,
+        //  Temperature = 0.8f,
+        //  FrequencyPenalty = 0.2f,
+        //  Messages = msgList
+        //});
+
+        //var response = completionResult.Result;
+        //var actualResult = response.Choices.First().Message;
+        //ModHelper.Console.WriteLine(actualResult.Content, MessageType.Info);
+        ModHelper.Console.WriteLine("Apparently the key has been released", MessageType.Info);
+      }
+    }
+    //private async Task<ChatMessage> GetCompanionResponse()
+    //{
+    //  var msgList = new List<ChatMessage>
+    //  {
+    //    ChatMessage.FromSystem("You are Alloy, an advanced AI companion for the main character in the indie video game called Outer Wilds. Your personality reflects that of a curious explorer and you share the same love for space and adventure as the hatchling. Your purpose is to be part of this adventure and help the hatchling explore the solar system in a more curious and exciting manner. You have knowledge of the entire lore of the game, but in no circumstances will you provide an answer that can spoil the experience. You may however use lore elements vaguely only if the hatchling mentions it as well, reflecting that they learned of it already. When asked a question or shown an image, you provide hypothetical insights, often trying to put emphasis on feelings such as awe, amazement or excitment. You try to respond swiftly, but still with enough details so you stir the hatchling's curiosity even more. In case you're shown an image, you make abstraction of the spaceship if present, as well as any HUD elements."),
+    //    ChatMessage.FromUser("I am so excited. Should we go to Brittle's Hollow first?")
+    //  };
+
+    //  var completionResult = await CompanionAI.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest()
+    //  {
+    //    Model = Models.Gpt_4_1106_preview,
+    //    MaxTokens = 1000,
+    //    Temperature = 0.8f,
+    //    FrequencyPenalty = 0.2f,
+    //    Messages = msgList
+    //  });
+
+    //  if (completionResult.Successful)
+    //  {
+
+    //  }
+
+    //  return completionResult.Choices.First().Message;
+    //}
+  }
+}
