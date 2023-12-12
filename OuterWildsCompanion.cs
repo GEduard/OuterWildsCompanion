de3b12795b3a3be5e79e2ae28dc1ad11996705a2
Eduard Gothard <gothard_eduard2000@yahoo.com>
2023-12-12 20:14:23 +0200

    Waiting for the chat response does not freeze the game anymore. Switched to Update call. API requests cannot be spammed.

--- OuterWildsCompanion.cs
+++ OuterWildsCompanion.cs
@@ -12,6 +12,7 @@ namespace OuterWildsCompanion
 {
   public class OuterWildsCompanion : ModBehaviour
   {
+    private bool requestInProgress = false;
     public GameObject companionObject = null;
     public static OuterWildsCompanion Instance;
 
@@ -27,8 +28,6 @@ namespace OuterWildsCompanion
     {
       // Starting here, you'll have access to OWML's mod helper.
       ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsCompanion)} is loaded!", MessageType.Success);
-      ModHelper.Console.WriteLine(Environment.GetEnvironmentVariable("OPENAI_API_KEY"), MessageType.Info);
-
 
       // Example of accessing game code.
       LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
@@ -39,28 +38,42 @@ namespace OuterWildsCompanion
 
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
       if (Keyboard.current[Key.V].wasReleasedThisFrame)
       {
-        IEnumerable<ChatRequestMessage> messages = new List<ChatRequestMessage>
+        if(!requestInProgress)
         {
-          new ChatRequestSystemMessage("You are Alloy, an advanced AI companion for the main character in the indie video game called Outer Wilds. Your personality reflects that of a curious explorer and you share the same love for space and adventure as the hatchling. Your purpose is to be part of this adventure and help the hatchling explore the solar system in a more curious and exciting manner. You have knowledge of the entire lore of the game, but in no circumstances will you provide an answer that can spoil the experience. You may however use lore elements vaguely only if the hatchling mentions it as well, reflecting that they learned of it already. When asked a question or shown an image, you provide hypothetical insights, often trying to put emphasis on feelings such as awe, amazement or excitment. You try to respond swiftly, but still with enough details so you stir the hatchling's curiosity even more. In case you're shown an image, you make abstraction of the spaceship if present, as well as any HUD elements."),
-          new ChatRequestUserMessage("I am so excited. Should we go to Brittle's Hollow first?")
-        };
-
-        var chatCompletionsOptions = new ChatCompletionsOptions("gpt-4-1106-preview", messages)
+          requestInProgress = true;
+          ModHelper.Console.WriteLine("Request to API sent!", MessageType.Success);
+          GetChatResponse();
+        }
+        else
         {
-          MaxTokens = 1000,
-          Temperature = 0.8f,
-          FrequencyPenalty = 0.2f,
-        };
-
-        var completionResult = CompanionAI.GetChatCompletionsAsync(chatCompletionsOptions);
-        var response = completionResult.Result;
-        var actualResult = response.Value.Choices[0].Message;
-        ModHelper.Console.WriteLine(actualResult.Content, MessageType.Info);
+          ModHelper.Console.WriteLine("A request is still in progress!", MessageType.Info);
+        }
       }
+    }
+
+    private async void GetChatResponse()
+    {
+      IEnumerable<ChatRequestMessage> messages = new List<ChatRequestMessage>
+      {
+        new ChatRequestSystemMessage("You are Alloy, an advanced AI companion for the main character in the indie video game called Outer Wilds. Your personality reflects that of a curious explorer and you share the same love for space and adventure as the hatchling. Your purpose is to be part of this adventure and help the hatchling explore the solar system in a more curious and exciting manner. You have knowledge of the entire lore of the game, but in no circumstances will you provide an answer that can spoil the experience. You may however use lore elements vaguely only if the hatchling mentions it as well, reflecting that they learned of it already. When asked a question or shown an image, you provide hypothetical insights, often trying to put emphasis on feelings such as awe, amazement or excitment. You try to respond swiftly, but still with enough details so you stir the hatchling's curiosity even more. In case you're shown an image, you make abstraction of the spaceship if present, as well as any HUD elements."),
+        new ChatRequestUserMessage("I am so excited. Should we go to Brittle's Hollow first?")
+      };
+
+      var chatCompletionsOptions = new ChatCompletionsOptions("gpt-4-1106-preview", messages)
+      {
+        MaxTokens = 1000,
+        Temperature = 0.8f,
+        FrequencyPenalty = 0.2f,
+      };
+
+      var completionResult = await CompanionAI.GetChatCompletionsAsync(chatCompletionsOptions);
+      var chatResponse = completionResult.Value.Choices[0].Message;
+      ModHelper.Console.WriteLine(chatResponse.Content, MessageType.Info);
+      requestInProgress = false;
     }
   }
 }
